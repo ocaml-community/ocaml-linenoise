37dc7cbc1db85b646c84f9a3153af1f580787c64
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-04-11 17:21:05 -0500

    fix(sefault): use caml named values for setting up hints/completion (closes #7)
    also use ref to store callbacks so they are registered to the GC

--- src/lNoise.ml
+++ src/lNoise.ml
@@ -2,8 +2,6 @@ type completions
 
 external add_completion : completions -> string -> unit = "ml_add_completion"
 
-external set_completion_callback :
-  (string -> completions -> unit) -> unit = "ml_set_completion_cb"
 external linenoise : string -> string option = "ml_linenoise"
 
 external history_add_ : string -> int = "ml_history_add"
@@ -13,8 +11,26 @@ external history_load_ : filename:string -> int = "ml_history_load"
 
 external catch_break : bool -> unit = "ml_catch_break"
 
+external setup_bridges : unit -> unit = "ml_setup_bridges"
+
+type hint_color = Red | Green | Yellow | Blue | Magenta | Cyan | White
+
+let completion_cb = ref (fun _ _ -> ())
+let hints_cb = ref (fun _ -> None)
+
+let set_completion_callback (f:string->completions->unit) : unit =
+  completion_cb := f;
+  Callback.register "lnoise_completion_cb" f
+
+let set_hints_callback (f:string -> (string*hint_color*bool) option) : unit =
+  hints_cb := f;
+  Callback.register "lnoise_hints_cb" f
+
 (* initialization: register [Sys.Break] and enable catch-break *)
 let () =
+  setup_bridges();
+  set_completion_callback !completion_cb;
+  set_hints_callback !hints_cb;
   Callback.register_exception "sys_break" Sys.Break;
   catch_break true
 
@@ -38,7 +54,3 @@ let history_load ~filename =
 external clear_screen : unit -> unit = "ml_clearscreen"
 external set_multiline : bool -> unit = "ml_set_multiline"
 external print_keycodes : unit -> unit = "ml_printkeycodes"
-
-type hint_color = Red | Green | Yellow | Blue | Magenta | Cyan | White
-external set_hints_callback :
-  (string -> (string * hint_color * bool) option) -> unit = "ml_set_hints_cb"
